9dd0cd1f042e11c8e6a50be3666334078dd5be9f
Bishwajit Sahu <bishwajit.sahu813@gmail.com>
2021-12-07 14:51:28 +0530

    removed the main method implementation

--- Hello.java
+++ Hello.java
@@ -1,22 +1,5 @@
 class Hello{
   public static void main(String args[]){
-    System.out.println("added");
-	System.out.println("hello");
-
-
-
-	System.out.println("something done in working directory to be commited.");
-
-	System.out.printl("from another user  commit");
-
-
-
-
-	System.out.println("added new codes in local");
-	  
-	System.out.println("added by dev-1");
-
-
-	System.out.println("local code changes");
+   
   }
 }
